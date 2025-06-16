f8d89524e51b5265583ca82ae7805bce3ada8f08
ilannakarla <ilannakarla@hotmail.com>
2025-06-15 23:47:49 -0300

    atualização do repositórios com os novos exercícios

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,17 @@
+public class Comentarios {
+    //olá, eu sou um comentário em uma única linha
+
+    /* olá,
+    *Eu sou um comentário
+    *que posso ser mais detalhado
+    *quando necessário
+    */
+
+    /**
+     * Estas duas estrelinhas acima
+     * servem para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+}
